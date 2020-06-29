ce673d8798bbee2a19b03f86521b58485daee0ed
Theo Winter <theo.winter@gmail.com>
2020-06-29 22:52:04 +0200

    Added logic to respawn player when they fall from the island, added debug window to show log output in vr, currently only shows position

---
+++ unity/RoomCreator/Assets/SixtyMeters/Scripts/RespawnBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnBehaviour : MonoBehaviour
+{
+
+    Vector3 spawnPosition = new Vector3(0, 100, 0);
+    float respawnThreshold = -100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DebugHelper.Instance.Log("Hello world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < respawnThreshold){
+            var player = GetComponentInChildren<OVRPlayerController>();
+            player.enabled = false;
+            transform.position = spawnPosition;
+            player.enabled = true;
+        }
+        DebugHelper.Instance.Log("Debug: "+transform.position);
+    }
+}
